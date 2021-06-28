d99ae4d06915a8229b5ad1cf2734924aa7179de6
JordanDF12 <jordan.florindez@gmail.com>
2021-06-28 13:03:39 -0500

    drawable elem funcional

--- app/src/main/java/com/miempresa/proyectofinal/MainActivity.kt
+++ app/src/main/java/com/miempresa/proyectofinal/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.miempresa.proyectofinal
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.StrictMode
+import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
@@ -55,13 +58,17 @@ class MainActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelect
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             R.id.inicio -> {
-                Toast.makeText(this, "Elegiste Inicio", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
             }
             R.id.Playas -> {
-                Toast.makeText(this, "Elegiste Playas", Toast.LENGTH_SHORT).show()
+                val actividad = Intent(applicationContext, playas_lugar::class.java)
+                startActivity(actividad)
+                finish()
             }
             R.id.regiones -> {
-                Toast.makeText(this, "Elegiste Regiones", Toast.LENGTH_SHORT).show()
+                val actividad = Intent(applicationContext, playas_lugar::class.java)
+                startActivity(actividad)
+                finish()
             }
             R.id.ayuda -> {
                 Toast.makeText(this, "Elegiste Ayuda", Toast.LENGTH_SHORT).show()
@@ -70,7 +77,17 @@ class MainActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelect
                 Toast.makeText(this, "Elegiste Configuraciones", Toast.LENGTH_SHORT).show()
             }
             R.id.salir -> {
-                Toast.makeText(this, "Elegiste Salir", Toast.LENGTH_SHORT).show()
+                val alert = AlertDialog.Builder(this)
+                alert.setTitle("Confirmacion de Salida..!!")
+                alert.setIcon(R.drawable.salir)
+                alert.setMessage("¿Estas seguro que quieres salir?")
+                alert.setCancelable(false)
+                alert.setPositiveButton(android.R.string.ok) {
+                        dialog, which ->
+                    finishAffinity()
+                }
+                alert.setNegativeButton("Cancelar", null)
+                alert.show()
             }
         }
         drawerLayout.closeDrawer(GravityCompat.START)
