6015bf8a2187dde72d787992f4730e51ac0ac0ed
Tibaran <palacios.tibaran@gmail.com>
2021-06-28 23:04:44 -0500

    "Edificios"

--- app/src/main/java/com/miempresa/proyectofinal/Restaurantes.kt
+++ app/src/main/java/com/miempresa/proyectofinal/Restaurantes.kt
@@ -4,16 +4,16 @@ import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_restaurantes.*
 import org.json.JSONException
 
 class Restaurantes : AppCompatActivity() {
-    private var nom :String?=""
-    private var desc :String?=""
-    private var cap :String?=""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_restaurantes)
@@ -24,32 +24,28 @@ class Restaurantes : AppCompatActivity() {
             cargarVista(bundle.getString("id_edificio").toString(),bundle.getString("edificio").toString() )
             cargarCapacidad(bundle.getString("edificio").toString())
         }
-        txtNombre.setText(nom)
-        txtDescripcion.setText(desc)
-        txtCapacidad.setText(cap)
+
 
     }
     fun cargarVista(id_edificio: String, n_edificio: String){
         AsyncTask.execute {
             val queue = Volley.newRequestQueue(applicationContext)
-            val url = resources.getString(R.string.API_IP)+"buildingsInterest/"+id_edificio+"/"
+            val url = resources.getString(R.string.API_IP)+"buildingsInterest"
             val stringRequest = JsonArrayRequest(url,
                 Response.Listener { response ->
                     try {
-
+                        for (i in 0 until response.length()){
+                            val id =
+                                response.getJSONObject(i).getString("id")
                             val nombre =
-                                response.getJSONObject(0).getString("nombreEI")
+                                response.getJSONObject(i).getString("nombreEI")
                             val descripcion =
-                                response.getJSONObject(0).getString("descripcion")
-                            val direccion =
-                                response.getJSONObject(0).getString("direccion")
-                            val contacto =
-                                response.getJSONObject(0).getString("contacto")
-
-                                nom = nombre
-                                desc = descripcion
-
-
+                                response.getJSONObject(i).getString("descripcion")
+                            if(id==id_edificio){
+                                txtNombre.setText(nombre)
+                                txtDescripcion.setText(descripcion)
+                            }
+                        }
                     } catch (e: JSONException) {
                         Toast.makeText(
                             applicationContext,
@@ -57,10 +53,10 @@ class Restaurantes : AppCompatActivity() {
                             Toast.LENGTH_LONG
                         ).show()
                     }
-                }, Response.ErrorListener {
+                }, Response.ErrorListener {response->
                     Toast.makeText(
                         applicationContext,
-                        "Revise su conexion a internet Info",
+                        "Verifica tu conexion a internet info",
                         Toast.LENGTH_LONG
                     ).show()
                 })
@@ -82,7 +78,7 @@ class Restaurantes : AppCompatActivity() {
                             val edificio =
                                 response.getJSONObject(i).getString("edificioInteres")
                             if(edificio == n_edificio) {
-                                cap = estado + "/" + capacidad
+                                txtCapacidad.setText(estado + "/" + capacidad)
                             }
                         }
 
